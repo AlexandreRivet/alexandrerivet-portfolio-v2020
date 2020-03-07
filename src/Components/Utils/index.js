ad05d6ccd52985d2fe51c230774a553c5201b4c1
Alexandre Rivet <5763611+AlexandreRivet@users.noreply.github.com>
2020-03-07 18:18:41 +0100

    Reset and restart + clean

--- src/Components/Utils/index.js
+++
@@ -1,3 +0,0 @@
-import { Dispatcher } from "./Event";
-
-export { Dispatcher };
--- src/Components/WebGL/World/index.js
+++
@@ -1,7 +0,0 @@
-import { Object3D } from "three";
-
-export default class World extends Object3D {
-  constructor(options) {
-    super();
-  }
-}
